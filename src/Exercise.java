1b5e484b2cc3cf241d1511f5261e1b1d0be17c00
earlbozarth <ehbbozarth@gmail.com>
2015-10-14 10:37:48 -0400

    Contacts with Morning Exercise

--- src/Exercise.java
+++ src/Exercise.java
@@ -6,22 +6,34 @@ import java.util.ArrayList;
 public class Exercise {
 
     public static void main(String[] args) {
+
+        Contact alice = new Contact();
+        alice.name = "Alice";
+        alice.age = 25;
+        Contact bob = new Contact();
+        bob.name = "Bob";
+        bob.age = 35;
+
+
+
         //ArrayList nameList = new ArrayList();
-        ArrayList<String> nameList = new ArrayList<>();
-        nameList.add("Alice");
-        nameList.add("Bob");
-        nameList.add("Charlie");
-        nameList.add("Austin");
+        //ArrayList<String> nameList = new ArrayList<>();
+        ArrayList<Contact> contacts = new ArrayList<>();
+        contacts.add(alice);
+        contacts.add(bob);
+        //nameList.add("Charlie");
+        //nameList.add("Austin");
 
         //String [] nameList = new String []{"Alice", "Bob", "Charlie", "Austin"};
 
-        for(String name : nameList){
+        //for(String name : nameList){
+        for (Contact contact : contacts){
 
-            if (name.startsWith("A")){
+            if (contact.name.startsWith("A")){
         //for (Object name : nameList){
             //String nameString = (String) name; *Casting an object variable
             //if(nameString.startsWith("A")){
-                System.out.println(name);
+                System.out.println(contact.name);
             }//End of if name starts with A
 
         }//End of for loop
