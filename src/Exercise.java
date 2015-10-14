6218f14c99187d2dba9bcc86a16fa303117cfa78
earlbozarth <ehbbozarth@gmail.com>
2015-10-14 09:44:21 -0400

    Contacts with Morning Exercise

---
+++ src/Exercise.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+/**
+ * Created by earlbozarth on 10/14/15.
+ */
+public class Exercise {
+
+    public static void main(String[] args) {
+        ArrayList<String> nameList = new ArrayList<>();
+        nameList.add("Alice");
+        nameList.add("Bob");
+        nameList.add("Charlie");
+        nameList.add("Austin");
+
+        for(String name : nameList){
+
+            if(name.startsWith("A")){
+                System.out.println(name);
+            }//End of if name starts with A
+
+        }//End of for loop
+
+    }//End of Main method
+
+}//End of Exercise Class
