2c3e45853d7e7aea0cc9dc27f4934f16d40da4a8
earlbozarth <ehbbozarth@gmail.com>
2015-10-19 10:29:23 -0400

    Exercise 3 with Nested For loops and Nested Data Structures

---
+++ src/Exercise3.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by earlbozarth on 10/19/15.
+ */
+public class Exercise3 {
+    public static void main(String[] args) {
+        String [] names = {"Alice", "Bob", "Charlie", "David"};
+        HashMap<String, ArrayList<Contact>> people = new HashMap<>();
+        //Key is the name, value is the contact object other than the person
+
+
+        for (String name : names) {
+            for (String name2 : names) {
+                if(!name.equals(name2)){
+                    //System.out.println(String.format("%s %s", name, name2));
+                    Contact person = new Contact(name2, 0);
+                    /*
+                    Contact person = new Contact();
+                    person.name = name2;
+                     */
+                    ArrayList<Contact> peopleList = people.get(name);
+                    if(peopleList == null){
+                        peopleList = new ArrayList<>();
+                        peopleList.add(person);
+                        people.put(name, peopleList);
+                    }//Enf of inner if statement
+                    else{
+                        peopleList.add(person);
+                    }
+                }//End of outer if statement
+            }//End of inner For Loop
+        }//End of outer For Loop
+        //Object Key is name, Value is name2
+
+        System.out.println();
+    }//End of Main Method
+}//End of Exercise 3 Class
